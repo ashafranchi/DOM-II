c265cd1af61a3d950bf7655de856927755cf8a5a
Asha <asha17@brown.edu>
2019-06-18 16:13:41 -0700

    drag, mouse-related, and double-click events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+let funImg = document.querySelector('.img-content');
+funImg.addEventListener('drag', function() {
+    alert('Coding is not a drag with events!');
+});
+
+let fBus = document.querySelector('h1');
+fBus.addEventListener('mouseover', function() {
+    fBus.textContent = 'Hop on the Fun Bus!';
+});
+fBus.addEventListener('mouseout', function() {
+    fBus.textContent = 'Fun Bus';
+});
+
+let destPick = document.querySelector('.content-destination');
+destPick.addEventListener('dblclick', function() {
+    destPick.textContent = 'Paris, Lisbon, Barcelona, or Munich';
+});
+destPick.addEventListener('dblclick', function() {
+    destPick.classList.add('destchange');
+});
